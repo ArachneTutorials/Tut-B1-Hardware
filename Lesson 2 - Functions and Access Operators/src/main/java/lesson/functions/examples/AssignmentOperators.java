8d380ef884bc55916c022a168eefb6e66d8f536c
Sean Zammit <smez12345@gmail.com>
2020-06-12 14:59:26 +1000

    Added assignment operators to lesson 2

---
+++ Lesson 2 - Functions and Access Operators/src/main/java/lesson/functions/examples/AssignmentOperators.java
@@ -0,0 +1,20 @@
+package lesson.functions.examples;
+
+public class AssignmentOperators {
+    public static void main(String[] args) {
+        
+        int x = 46;
+        int y = 13;
+
+        x++;
+        x--;
+
+        x += y;
+        x -= y;
+        x *= y;
+        x /= y;
+        x %= y;
+
+        System.out.println(x);
+    }
+}
